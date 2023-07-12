cd6a3e0ad0e57b46ebe9d8df6053fc676fa30b82
prahladchandra <prahlad@arcgate.com>
2023-07-12 18:01:54 +0530

    space added

--- script.js
+++ script.js
@@ -5,47 +5,49 @@ const nextBtn = document.querySelector('.nextBtn');
 const scoreCard = document.querySelector('.scoreCard');
 const alert = document.querySelector('.alert');
 const startBtn = document.querySelector('.startBtn');
-// Make an array of objects that stores question.
+// array of objects that stores question.
 const quiz = [
     {
-        question: "Q.What year was Javascript launched?",
+        question: "1.What year was Javascript launched?",
         choices: ["1998", "1995", "1994", "Non of these"],
-        answer: "border-collapse"
+        answer: "1995"
     },
     {
-        question: "Q. Which of the following is not a valid way to declare a function in JavaScript?",
-        choices: ["function myFunction() {}", " let myFunction = function() {};", "myFunction: function() {}", "const myFunction = () => {};"],
-        answer: "myFunction: function() {}"
+        question: "2.What does HTML stand for?",
+        choices: ["Hypertext Markup Language", " Hyperthext Markdown Language", "Hyperloop Machine Language", "const myFunction = () => {};"],
+        answer: "Hypertext Markup Language"
     },
     {
-        question: "Q. Which of the following is not a JavaScript data type?",
-        choices: ["string", "boolean", "object", "float"],
-        answer: "float"
+        question: "3.What does CSS stand for?",
+        choices: ["C", "Python", "java", "JavaScript"],
+        answer: "JavaScript"
     },
     {
-        question: "Q. What is the purpose of the this keyword in JavaScript?",
+        question: "4.What is the purpose of the this keyword in JavaScript?",
         choices: ["It refers to the current function.", "It refers to the current object.", "It refers to the parent object.", " It is used for comments."],
         answer: "It refers to the current object."
+    },
+    {
+        question: "5.What are the features of JavaScript?",
+        choices: ["Open Source", "Lightweight", "Non of these.", "platefrom-based applications"],
+        answer: "Open Source"
     }
 ];
-
-// Making Variables
 let currentQuestionIndex = 0;
-let score = 0;
+let totalscore = 0;
 let quizOver = false;
-// Arrow Function for showing  Show Questions
+// Arrow Function for showing Questions
 const showQuestions = () => {
-    const questionDetails = quiz[currentQuestionIndex];
+    const questionDetails = quiz[currentQuestionIndex];   
     questionBox.textContent = questionDetails.question;
-
     choicesBox.textContent = "";
+    console.log(choicesBox.textContent)
     for (let i = 0; i < questionDetails.choices.length; i++) {
         const currentChoice = questionDetails.choices[i];
         const choiceDiv = document.createElement('div');
         choiceDiv.textContent = currentChoice;
         choiceDiv.classList.add('choice');
         choicesBox.appendChild(choiceDiv);
-
         choiceDiv.addEventListener('click', () => {
             if (choiceDiv.classList.contains('selected')) {
                 choiceDiv.classList.remove('selected');
@@ -56,14 +58,11 @@ const showQuestions = () => {
         });
     }
 }
-
 // Function to check answers
 const checkAnswer = () => {
     const selectedChoice = document.querySelector('.choice.selected');
     if (selectedChoice.textContent === quiz[currentQuestionIndex].answer) {
-        // alert("Correct Answer!");
-        displayAlert("Correct Answer!");
-        score++;
+               totalscore++;
     }
     currentQuestionIndex++;
     if (currentQuestionIndex < quiz.length) {
@@ -73,28 +72,24 @@ const checkAnswer = () => {
         showScore();
     }
 }
-
 // Function to show score
 const showScore = () => {
     questionBox.textContent = "";
     choicesBox.textContent = "";
-    scoreCard.textContent = `You Scored ${score} out of ${quiz.length}!`;
-    displayAlert("You have completed this quiz!");
+    scoreCard.textContent = `You Scored ${totalscore} out of ${quiz.length}!`;
+    displayAlert("Woo-Woo! you have completed this quiz!");
     nextBtn.textContent = "Play Again";
     quizOver = true;
 }
-
 // Function to Show Alert
 const displayAlert = (msg) => {
-    alert.style.display = "block";
+    // alert.style.display = "block";
     alert.textContent = msg;
     setTimeout(()=>{
-        alert.style.display = "none";
-    }, 2000);
+        }, 2000);
 }
-
-// Function to shuffle question
-const shuffleQuestions = () =>{
+// random question throw.
+const scrambleQuestions = () =>{
     for(let i=quiz.length-1; i>0; i--){
         const j = Math.floor(Math.random() * (i+1));
         [quiz[i], quiz[j]] = [quiz[j], quiz[i]];
@@ -102,20 +97,17 @@ const shuffleQuestions = () =>{
     currentQuestionIndex = 0;
     showQuestions();
 }
-
-// Function to Start Quiz
+// Start Quiz function
 const startQuiz = () =>{
-     shuffleQuestions();
+    scrambleQuestions();
 }
-
 // Adding Event Listener to Start Button
 startBtn.addEventListener('click', ()=>{
     startBtn.style.display = "none";
     container.style.display = "block";
     startQuiz();
 });
-
-nextBtn.addEventListener('click', () => {
+function handleClick() {
     const selectedChoice = document.querySelector('.choice.selected');
     if (!selectedChoice && nextBtn.textContent === "Submit") {
         alert("Select your answer");
@@ -129,8 +121,10 @@ nextBtn.addEventListener('click', () => {
         quizOver = false;
         score = 0;
         startQuiz();
-    }
-    else {
+    } else {
         checkAnswer();
     }
-});
+}
+nextBtn.addEventListener('click', handleClick);
+
+
